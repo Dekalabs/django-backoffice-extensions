86fa285a2f43a7bdc570624e7e66cc37bf8d40a0
Roberto Broseta Vigara <roberto@dekalabs.com>
2022-08-10 10:31:26 +0200

    fix: Fixed boolean icon

--- backoffice_extensions/templatetags/backoffice.py
+++ backoffice_extensions/templatetags/backoffice.py
@@ -66,7 +66,7 @@ def boolean_icon(value):
     result = f"""
     <span class="{'text-green-600' if value else 'text-red-500' }">
         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="{'M6 18L18 6M6 6l12 12' if value else 'M6 18L18 6M6 6l12 12' }" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="{'M5 13l4 4L19 7' if value else 'M6 18L18 6M6 6l12 12' }" />
         </svg>
     </span>
     """
