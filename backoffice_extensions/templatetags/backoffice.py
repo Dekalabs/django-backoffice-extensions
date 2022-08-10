a4092d5b76cfb8f8937b7a91866e4194c958972d
Roberto Broseta Vigara <roberto@dekalabs.com>
2022-08-10 10:23:12 +0200

    fix: Changed boolean icon to match to match its correct color and icon

--- backoffice_extensions/templatetags/backoffice.py
+++ backoffice_extensions/templatetags/backoffice.py
@@ -64,9 +64,9 @@ def sidebar_menu(context):
 def boolean_icon(value):
     """Gets an icon for given boolean value."""
     result = f"""
-    <span class="{'text-red-500' if value else 'text-green-600' }">
+    <span class="{'text-green-600' if value else 'text-red-500' }">
         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="{'M6 18L18 6M6 6l12 12' if value else 'M5 13l4 4L19 7' }" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="{'M6 18L18 6M6 6l12 12' if value else 'M6 18L18 6M6 6l12 12' }" />
         </svg>
     </span>
     """
